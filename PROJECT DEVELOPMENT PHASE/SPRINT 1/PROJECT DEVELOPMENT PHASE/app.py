6037c6bd3bebf8a33e627998b08723931bada1d9
Ansalnaans <114214207+Ansalnaans@users.noreply.github.com>
2022-11-14 11:34:58 +0530

    Add files via upload

---
+++ PROJECT DEVELOPMENT PHASE/SPRINT 1/PROJECT DEVELOPMENT PHASE/app.py
@@ -0,0 +1,59 @@
+import os
+
+import numpy as np
+from PIL import Image
+from flask import Flask, request, render_template
+from keras.models import load_model
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'C:/Users/Aadi/PycharmProject/application/uploads'
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+model = load_model("mnistCNN.h5")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == "POST":
+        f = request.files["image"]
+        filepath = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filepath))
+
+        upload_img = os.path.join(UPLOAD_FOLDER, filepath)
+        img = Image.open(upload_img).convert("L")  # convert image to monochrome
+        img = img.resize((28, 28))  # resizing of input image
+
+        im2arr = np.array(img)  # converting to image
+        im2arr = im2arr.reshape(1, 28, 28, 1)  # reshaping according to our requirement
+
+        pred = model.predict(im2arr)
+
+        num = np.argmax(pred, axis=1)  # printing our Labels
+
+        return render_template('predict.html', num=str(num[0]))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=False)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
